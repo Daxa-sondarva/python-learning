015c54c4479300023188bdb2389d2455b5229200
Daxa Sondarva <daxasondarva2208@gmail.com>
2025-07-03 18:39:24 +0530

    day 5 - Max of three Number

---
+++ day5_max_of_three.py
@@ -0,0 +1,12 @@
+#Maximum of Three Numbers
+
+a = int(input("Enter first: "))
+b = int(input("Enter second: "))
+c = int(input("Enter third: "))
+
+if a >= b and a >= c:
+    print("Max is", a)
+elif b >= a and b >= c:
+    print("Max is", b)
+else:
+    print("Max is", c)
